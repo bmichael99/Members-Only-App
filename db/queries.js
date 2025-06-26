5213c6ccbeb4600fca9b84c175d3a7c8b21887d1
Michael <michaelbrandmeyer99@gmail.com>
2025-06-26 16:25:29 -0700

    added queries for createuser and createMessage

--- db/queries.js
+++ db/queries.js
@@ -1,12 +1,23 @@
 const pool = require("./pool");
 
-async function createUser(username,password) {
-  await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+async function createUser(first_name,last_name,username,password) {
+  await pool.query("INSERT INTO users (first_name, last_name, username, password ) VALUES ($1, $2, $3, $4)", [
+      first_name,
+      last_name,
       username,
       password,
     ]);
 }
 
+async function createMessage(title,content,user_id) {
+  await pool.query("INSERT INTO messages (title, content, user_id) VALUES ($1, $2, $3)", [
+      title,
+      content,
+      user_id,
+    ]);
+}
+
 module.exports = {
   createUser,
+  createMessage,
 };
